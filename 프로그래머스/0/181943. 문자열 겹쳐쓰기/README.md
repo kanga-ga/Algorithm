82617047dd604db1082db0036f4289febceb419b
kanga-ga <gtg10291@naver.com>
2024-11-23 23:24:04 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 2.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 77.1 MB, 시간: 2.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 23일 23:08:37
+2024년 11월 23일 23:24:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,17 +1,12 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
-        int len1= overwrite_string.length();
-        int len2= my_string.length();   
-        String str1 =my_string.substring(0,s);
-        String str2 =my_string.substring(s+len1,len2);
-        answer += str1+overwrite_string+str2;
-        
+        int len1 = my_string.length();
+        int len2 = overwrite_string.length();
+        String str1 = my_string.substring(0,s);
+        String str2 = overwrite_string + my_string.substring(s+len2,len1) ;
+        answer = str1 +str2;
         
         return answer;
     }
-   
-        
-        
-    
 }

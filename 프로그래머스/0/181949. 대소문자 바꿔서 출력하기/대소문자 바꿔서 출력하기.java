3e293b707fa514bf3d90666153c2b6b13e7593a2
kanga-ga <gtg10291@naver.com>
2024-11-23 16:09:15 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 175.39 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -4,14 +4,16 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        
-        for(int i=0 ; i<a.length(); i++){
-            char c= a.charAt(i);
-            if(Character.isUpperCase(c))
-                System.out.print(Character.toLowerCase(c));
-            else System.out.print(Character.toUpperCase(c));
-                
+        String result ="";
+        for(char c: a.toCharArray()){
+            if(c== Character.toLowerCase(c)){
+                result +=Character.toUpperCase(c);
+            } else{
+                result+= Character.toLowerCase(c);
+            }
         }
+   System.out.print(result);
+        
     
     }
 }

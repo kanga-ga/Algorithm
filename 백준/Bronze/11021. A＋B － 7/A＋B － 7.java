cc840cc6c222893995f971fc92dec38451c9de8c
kanga-ga <gtg10291@naver.com>
2025-05-09 15:12:18 +0900

    [Bronze V] Title: A+B - 7, Time: 208 ms, Memory: 18620 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        
+        for (int i = 1; i <=a ; i++) {
+            int b = input.nextInt();
+            int c = input.nextInt();
+            System.out.println("Case #"+i+": "+ (b+c));
+
+
+        }
+    }
+}

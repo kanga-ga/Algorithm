9e964663af459b5c9de2e2c42554cc46791eb615
kanga-ga <gtg10291@naver.com>
2025-05-13 14:02:40 +0900

    [Bronze V] Title: A+B - 8, Time: 240 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -5,11 +5,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int T = input.nextInt();
 
-        for (int i = 1; i <= T; i++) {
+        for (int i = 1; i <=T ; i++) {
             int a = input.nextInt();
             int b = input.nextInt();
-            int sum = a + b;
-            System.out.printf("Case #%d: %d + %d = %d%n", i, a, b, sum); // ← 여기!
+            int sum = a+b;
+            System.out.printf("Case #%d: %d + %d = %d%n",i,a,b,sum);
+
         }
     }
 }
+

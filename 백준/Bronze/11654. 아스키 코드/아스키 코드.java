0a23b0d95b4b984186aacb89a3893d3e026a61fe
kanga-ga <gtg10291@naver.com>
2025-05-16 14:42:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        char ch = s.charAt(0);
+        System.out.println((int)ch);
+    }
+}

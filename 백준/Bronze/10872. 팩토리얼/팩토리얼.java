ee1f4a34e93b711fa6da1f0544686efd326bc4e6
kanga-ga <gtg10291@naver.com>
2025-05-16 15:21:08 +0900

    [Bronze III] Title: 팩토리얼, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int total =1;
+        for (int i = 1; i <=a ; i++) {
+            total = total *i;
+
+        }
+        System.out.println(total);
+    }
+}

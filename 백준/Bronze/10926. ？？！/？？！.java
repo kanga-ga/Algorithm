cf217d5ed1d8a94fe89dda6027f2c63657040a74
kanga-ga <gtg10291@naver.com>
2025-05-16 14:46:32 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String id = input.nextLine();
+        System.out.println(id+"??!");
+
+    }
+}

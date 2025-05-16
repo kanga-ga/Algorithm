b3e9b04d897437ca4e9b1f6ed214bb0cdee4b34c
kanga-ga <gtg10291@naver.com>
2025-05-16 16:09:58 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+        int e = input.nextInt();
+        int f = input.nextInt();
+        System.out.println(1-a);
+        System.out.println(1-b);
+        System.out.println(2-c);
+        System.out.println(2-d);
+        System.out.println(2-e);
+        System.out.println(8-f);
+
+
+    }
+}
